a8a0ba12ebd726d75bf4e0df755ea13ac236a4af
patajotss <mhhdhabib21@gmail.com>
2024-03-15 05:27:50 +0700

    gud

--- MainsPackage2/BukuPerpus/JavaLibrary.java
+++ MainsPackage2/BukuPerpus/JavaLibrary.java
@@ -1,4 +1,5 @@
 package MainsPackage2.BukuPerpus;
+
 import java.util.Scanner;
 
 public class JavaLibrary {
@@ -8,39 +9,39 @@ public class JavaLibrary {
 
         // Menambahkan buku-buku untuk setiap genre
         // Genre: Politik
-        library.addBook(0, 0, "Politik dan Pembangunan", "Mohammad Hatta", 350, "Gramedia");
-        library.addBook(0, 1, "Perjuangan Kemerdekaan", "Ir. Soekarno", 450, "Erlangga");
-        library.addBook(0, 2, "Demokrasi Modern", "Abraham Lincoln", 280, "Pustaka Jaya");
-        library.addBook(0, 3, "Teori Politik", "Niccolo Machiavelli", 320, "Penerbit ITB");
-        library.addBook(0, 4, "Negara dan Ideologi", "John Locke", 400, "Penerbit Universitas Indonesia");
+        library.addBook(0, 0, "Politik dan Pembangunan", "Mohammad Hatta", 350, "Gramedia", "politik dan pembangunan yang membahas peran politik dalam proses pembangunan sebuah negara.");
+        library.addBook(0, 1, "Perjuangan Kemerdekaan", "Ir. Soekarno", 450, "Erlangga", "tentang perjuangan bangsa Indonesia dalam merebut kemerdekaannya.");
+        library.addBook(0, 2, "Demokrasi Modern", "Abraham Lincoln", 280, "Pustaka Jaya", "mengenai prinsip-prinsip demokrasi modern dan penerapannya di berbagai negara.");
+        library.addBook(0, 3, "Teori Politik", "Niccolo Machiavelli", 320, "Penerbit ITB", "teori politik dari sudut pandang Machiavelli yang kontroversial.");
+        library.addBook(0, 4, "Negara dan Ideologi", "John Locke", 400, "Penerbit Universitas Indonesia", "tentang hubungan antara negara dan ideologi menurut pemikiran John Locke.");
 
         // Genre: Sains
-        library.addBook(1, 0, "Sains Alamiah", "Stephen Hawking", 500, "Springer");
-        library.addBook(1, 1, "Kimia Modern", "Marie Curie", 380, "Wiley");
-        library.addBook(1, 2, "Fisika Kuantum", "Albert Einstein", 450, "McGraw-Hill");
-        library.addBook(1, 3, "Biologi Sel", "Charles Darwin", 420, "Pearson");
-        library.addBook(1, 4, "Astronomi Dasar", "Galileo Galilei", 360, "Elsevier");
+        library.addBook(1, 0, "Sains Alamiah", "Stephen Hawking", 500, "Springer", "mengenai konsep-konsep dasar dalam sains alamiah dan pemahaman tentang alam semesta.");
+        library.addBook(1, 1, "Kimia Modern", "Marie Curie", 380, "Wiley", "tentang perkembangan ilmu kimia modern dan kontribusi Marie Curie di bidang ini.");
+        library.addBook(1, 2, "Fisika Kuantum", "Albert Einstein", 450, "McGraw-Hill", "tentang teori fisika kuantum dan kontribusi Einstein dalam pengembangannya.");
+        library.addBook(1, 3, "Biologi Sel", "Charles Darwin", 420, "Pearson", "mengenai struktur dan fungsi biologi sel serta teori evolusi Darwin.");
+        library.addBook(1, 4, "Astronomi Dasar", "Galileo Galilei", 360, "Elsevier", "tentang konsep dasar astronomi dan penemuan-penemuan Galileo Galilei.");
 
         // Genre: Sejarah
-        library.addBook(2, 0, "Sejarah Peradaban", "Will Durant", 600, "HarperCollins");
-        library.addBook(2, 1, "Perang Dunia I", "Winston Churchill", 480, "Oxford University Press");
-        library.addBook(2, 2, "Sejarah Kuno", "Herodotus", 520, "Penguin Classics");
-        library.addBook(2, 3, "Kolonialisme dan Imperialisme", "Frantz Fanon", 420, "Verso");
-        library.addBook(2, 4, "Sejarah Modern", "Eric Hobsbawm", 550, "Verso");
+        library.addBook(2, 0, "Sejarah Peradaban", "Will Durant", 600, "HarperCollins", "perjalanan sejarah peradaban manusia dari masa purba hingga zaman modern.");
+        library.addBook(2, 1, "Perang Dunia I", "Winston Churchill", 480, "Oxford University Press", "tentang peristiwa dan dampak Perang Dunia I dalam sejarah dunia.");
+        library.addBook(2, 2, "Sejarah Kuno", "Herodotus", 520, "Penguin Classics", "mengenai catatan sejarah kuno dari sudut pandang Herodotus.");
+        library.addBook(2, 3, "Kolonialisme dan Imperialisme", "Frantz Fanon", 420, "Verso", "tentang kolonialisme dan imperialisme serta dampaknya terhadap bangsa-bangsa di seluruh dunia.");
+        library.addBook(2, 4, "Sejarah Modern", "Eric Hobsbawm", 550, "Verso", "mengenai perkembangan sejarah dunia pada era modern dan perubahan sosial yang terjadi.");
 
         // Genre: Fiksi
-        library.addBook(3, 0, "Matahari", "Tere Liye", 320, "Republika");
-        library.addBook(3, 1, "Laskar Pelangi", "Andrea Hirata", 390, "Bentang Pustaka");
-        library.addBook(3, 2, "Perahu Kertas", "Dee Lestari", 350, "Bentang Pustaka");
-        library.addBook(3, 3, "Bumi Manusia", "Pramoedya Ananta Toer", 420, "Hasta Mitra");
-        library.addBook(3, 4, "Ayat-Ayat Cinta", "Habiburrahman El Shirazy", 360, "Republika");
+        library.addBook(3, 0, "Matahari", "Tere Liye", 320, "Republika", "tentang perjalanan seorang tokoh dalam menghadapi berbagai rintangan hidup.");
+        library.addBook(3, 1, "Laskar Pelangi", "Andrea Hirata", 390, "Bentang Pustaka", "tentang perjuangan sekelompok anak di daerah terpencil untuk mendapatkan pendidikan yang layak.");
+        library.addBook(3, 2, "Perahu Kertas", "Dee Lestari", 350, "Bentang Pustaka", "tentang kisah cinta dan petualangan dua tokoh utama yang tidak biasa.");
+        library.addBook(3, 3, "Bumi Manusia", "Pramoedya Ananta Toer", 420, "Hasta Mitra", "tentang perjuangan seorang pribumi dalam menghadapi penjajahan Belanda di Indonesia.");
+        library.addBook(3, 4, "Ayat-Ayat Cinta", "Habiburrahman El Shirazy", 360, "Republika", "tentang perjalanan seorang pria dalam mencari makna cinta sejati dalam agama Islam.");
 
         // Genre: Romance
-        library.addBook(4, 0, "Me Before You", "Jojo Moyes", 320, "Penguin Books");
-        library.addBook(4, 1, "The Notebook", "Nicholas Sparks", 350, "Warner Books");
-        library.addBook(4, 2, "Pride and Prejudice", "Jane Austen", 400, "Penguin Classics");
-        library.addBook(4, 3, "Romeo and Juliet", "William Shakespeare", 320, "Penguin Books");
-        library.addBook(4, 4, "A Walk to Remember", "Nicholas Sparks", 280, "Warner Books");
+        library.addBook(4, 0, "Me Before You", "Jojo Moyes", 320, "Penguin Books", "tentang kisah cinta yang penuh liku antara dua tokoh dengan latar belakang yang berbeda.");
+        library.addBook(4, 1, "The Notebook", "Nicholas Sparks", 350, "Warner Books", "tentang kisah cinta abadi antara dua insan yang dipisahkan oleh waktu dan keadaan.");
+        library.addBook(4, 2, "Pride and Prejudice", "Jane Austen", 400, "Penguin Classics", "tentang perjuangan tokoh utama mengatasi rasa bangga dan prasangka dalam mencari cinta sejati.");
+        library.addBook(4, 3, "Romeo and Juliet", "William Shakespeare", 320, "Penguin Books", "tentang cinta tragis antara dua keluarga yang berseteru.");
+        library.addBook(4, 4, "A Walk to Remember", "Nicholas Sparks", 280, "Warner Books", "tentang perjalanan cinta yang menginspirasi antara dua remaja dengan masa lalu yang penuh warna.");
 
         // Meminta pengunjung untuk memasukkan kode genre dan indeks buku yang ingin diakses
         Scanner scanner = new Scanner(System.in);
@@ -52,17 +53,30 @@ public class JavaLibrary {
         // Menampilkan detail buku berdasarkan kode genre dan indeks buku
         library.getBookDetails(genreCode - 1, bookIndex - 1);
 
+        // Menampilkan detail buku berdasarkan kode genre dan indeks buku
+        library.getBookDetails(genreCode - 1, bookIndex - 1);
+
+        // Membuat salinan buku yang dipilih
+        Book selectedBook = library.getBook(genreCode - 1, bookIndex - 1).copy();
+        System.out.println("\nCopied Book Details:");
+        selectedBook.printDetails();
+
+        // Menghitung jumlah kata pada sinopsis buku yang dipilih
+        int wordCount = library.getSinopsisWordCount(genreCode - 1, bookIndex - 1);
+        System.out.println("Number of words in sinopsis: " + wordCount);
+
+        // Menampilkan detail buku kedua berdasarkan kode genre dan indeks buku
+        library.getBookDetails(genreCode - 1, bookIndex - 1);
+
+        // Membandingkan kesamaan antara dua buku
+        Book firstBook = library.getBook(genreCode - 1, bookIndex - 1);
+        Book secondBook = library.getBook(genreCode - 1, bookIndex - 1);
+
+        if (firstBook != null && secondBook != null) {
+            double similarity = firstBook.cekTingkatKesamaan(secondBook);
+            System.out.println("Similarity between the two books: " + similarity + "%");
+        }
+
         scanner.close();
     }
-    public void showtype(String[][] type, String categoryType) {
-        System.out.printf("Kategori %s\n\n", categoryType);
-        for (int i = 0; i < type.length; i++) {
-                System.out.printf("Buku ke-%d\n"
-                                + "1. %-15s: %s\n" + "2. %-15s: %s\n" + "3. %-15s: %s halaman\n"
-                                + "4. %-15s: %s\n" + "5. %-15s: %s\n\n",
-                                i + 1, "Judul Buku", type[i][0], "Penulis", type[i][1], "Jumlah Halaman",
-                                type[i][2],
-                                "Penerbit", type[i][3], "Tahun Terbit", type[i][4]);
-        }
-}
 }
--- MainsPackage2/BukuPerpus/Perpus.java
+++ MainsPackage2/BukuPerpus/Perpus.java
@@ -1,54 +1,130 @@
 package MainsPackage2.BukuPerpus;
 import java.util.Scanner;
 
-// Kelas untuk merepresentasikan buku
+// Class untuk merepresentasikan buku
 class Book {
     private String title;
     private String author;
     private int pageCount;
     private String publisher;
+    private String sinopsis; // Penambahan atribut sinopsis
 
-    public Book(String title, String author, int pageCount, String publisher) {
-        this.title = title;
-        this.author = author;
-        this.pageCount = pageCount;
-        this.publisher = publisher;
+    // Constructor untuk membuat objek Book dengan judul, penulis, jumlah halaman, penerbit, dan sinopsis tertentu
+    public Book(String title, String author, int pageCount, String publisher, String sinopsis) {
+        this.title = title;                 // Inisialisasi atribut judul
+        this.author = author;               // Inisialisasi atribut penulis
+        this.pageCount = pageCount;         // Inisialisasi atribut jumlah halaman
+        this.publisher = publisher;         // Inisialisasi atribut penerbit
+        this.sinopsis = sinopsis;           // Inisialisasi atribut sinopsis
+    }
+    // Constructor untuk membuat objek Book dengan nilai atribut yang sama dari objek lain
+    public Book(Book other) {
+        this.title = other.title;
+        this.author = other.author;
+        this.pageCount = other.pageCount;
+        this.publisher = other.publisher;
+        this.sinopsis = other.sinopsis;
     }
 
-    // Metode untuk mencetak detail buku
+    // Method untuk membuat salinan objek Book
+    public Book copy() {
+        return new Book(this);
+    }
+
+    // Method untuk menghitung persentase kesamaan antara dua buku
+    public double cekTingkatKesamaan(Book otherBook) {
+        // Hitung persentase kesamaan untuk setiap atribut
+        double titleSimilarity = calculateSimilarity(this.title, otherBook.title);
+        double authorSimilarity = calculateSimilarity(this.author, otherBook.author);
+        double pageCountSimilarity = calculateSimilarity(this.pageCount, otherBook.pageCount);
+        double publisherSimilarity = calculateSimilarity(this.publisher, otherBook.publisher);
+        double sinopsisSimilarity = calculateSimilarity(this.sinopsis, otherBook.sinopsis);
+
+        // Hitung rata-rata persentase kesamaan
+        double averageSimilarity = (titleSimilarity + authorSimilarity + pageCountSimilarity + publisherSimilarity + sinopsisSimilarity) / 5.0;
+
+        return averageSimilarity * 100; // Konversi ke persentase
+    }
+
+    // Metode untuk menghitung persentase kesamaan antara dua nilai
+    private double calculateSimilarity(String value1, String value2) {
+        // Gunakan metode pembanding yang sesuai di sini.
+        // Misalnya, Anda dapat menggunakan algoritma Levenshtein distance atau Jaccard similarity.
+        // Di sini, untuk kesederhanaan, kita akan menggunakan equalsIgnoreCase.
+        return value1.equalsIgnoreCase(value2) ? 1.0 : 0.0;
+    }
+
+    // Metode untuk menghitung persentase kesamaan antara dua nilai numerik
+    private double calculateSimilarity(int value1, int value2) {
+        // Misalnya, Anda dapat menghitung persentase perbedaan antara dua nilai numerik.
+        // Di sini, untuk kesederhanaan, kita akan menggunakan perbandingan langsung.
+        return value1 == value2 ? 1.0 : 0.0;
+    }
+
+    // Method untuk mencetak detail buku
     public void printDetails() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("Page Count: " + pageCount);
-        System.out.println("Publisher: " + publisher);
+        System.out.println("Title: " + title);             // Menampilkan judul
+        System.out.println("Author: " + author);           // Menampilkan penulis
+        System.out.println("Page Count: " + pageCount);    // Menampilkan jumlah halaman
+        System.out.println("Publisher: " + publisher);     // Menampilkan penerbit
+        System.out.println("Sinopsis: " + sinopsis);       // Menampilkan sinopsis
+    }
+    // Getter method untuk mendapatkan sinopsis buku
+    public String getSinopsis() {
+        return sinopsis;
+    }
+    // Method untuk menghitung jumlah kata pada sinopsis
+    public int countWordsInSinopsis() {
+        String[] words = sinopsis.split("\\s+");
+        return words.length;
     }
 }
 
-// Kelas untuk merepresentasikan perpustakaan
+// Class untuk merepresentasikan perpustakaan
 class Library {
     private Book[][] books; // Array 2D untuk menyimpan buku-buku
 
-    // Konstruktor untuk membuat perpustakaan dengan jumlah genre dan buku yang ditentukan
+    // Constructor untuk membuat perpustakaan dengan jumlah genre dan buku yang ditentukan
     public Library(int numGenres, int booksPerGenre) {
         books = new Book[numGenres][booksPerGenre];
     }
 
-    // Metode untuk menambahkan buku ke perpustakaan
-    public void addBook(int genre, int index, String title, String author, int pageCount, String publisher) {
+    // Method untuk menambahkan buku ke perpustakaan
+    public void addBook(int genre, int index, String title, String author, int pageCount, String publisher, String sinopsis) {
         if (genre >= 0 && genre < books.length && index >= 0 && index < books[genre].length) {
-            books[genre][index] = new Book(title, author, pageCount, publisher);
+            books[genre][index] = new Book(title, author, pageCount, publisher, sinopsis);
         } else {
             System.out.println("Invalid genre or index");
         }
     }
+    // Method untuk mendapatkan buku berdasarkan kode genre dan indeks buku
+    public Book getBook(int genre, int index) {
+        if (genre >= 0 && genre < books.length && index >= 0 && index < books[genre].length) {
+            return books[genre][index];
+        } else {
+            System.out.println("Book not found");
+            return null; // Return null if book not found
+        }
+    }
 
-    // Metode untuk mencetak detail buku berdasarkan kode genre dan indeks buku
+    // Method untuk mencetak detail buku berdasarkan kode genre dan indeks buku
     public void getBookDetails(int genre, int index) {
         if (genre >= 0 && genre < books.length && index >= 0 && index < books[genre].length && books[genre][index] != null) {
             System.out.println("Genre " + (genre + 1) + ", Book " + (index + 1) + " Details:");
             books[genre][index].printDetails();
         } else {
             System.out.println("Book not found");
+        }
+    }
+    // Metode untuk menghitung jumlah kata pada sinopsis buku berdasarkan kode genre dan indeks buku
+    public int getSinopsisWordCount(int genre, int index) {
+        if (genre >= 0 && genre < books.length && index >= 0 && index < books[genre].length && books[genre][index] != null) {
+            String sinopsis = books[genre][index].getSinopsis();
+            String[] words = sinopsis.split("\\s+");
+            return words.length;
+        } else {
+            System.out.println("Book not found");
+            return -1; // Return -1 if book not found
         }
     }
 }
